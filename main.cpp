7fe6df2538dbe38120d1c0b5226c83659194301b
Hatice Can <haticecan297@gmail.com>
2026-04-24 10:42:45 +0300

    Kütüphane kurulumu sonrası ilk commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    // VideoMode artýk süslü parantez {} ile vektör bekler
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML 3.0 Test");
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    // HATA ALDIÐIN YER: setPosition artýk ({x, y}) þeklinde yazýlmalý
+    shape.setPosition({300.f, 200.f});
+
+    while (window.isOpen()) {
+        // SFML 3.0'da pollEvent kullanýmý da deðiþti (Opsiyonel ama önerilen)
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
